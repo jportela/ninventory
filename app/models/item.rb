8d168a238983bdc23fa8a34484a6d4d7798d0b13
João Portela <joao.portela@gmail.com>
2011-10-09 20:09:24 +0100

    Adicionado categorias e status code

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   
   CATEGORIES = ['Livros', 'Software', 'Hardware', 'Outros']
-  STATUS_CODES = ["Disponível", "Requisitado", "Perdido / Estragado"]
+  STATUS_CODES = ["Disponivel", "Requisitado", "Perdido / Estragado"]
   
   validates_presence_of :name, :message => "O nome tem de ser inserido."
   validates_inclusion_of :category, :in => CATEGORIES
