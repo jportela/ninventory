1dcf428a5a37e71b0c08f600fc8f4283e8f789d2
João Portela <joao.portela@gmail.com>
2011-10-09 18:41:01 +0100

    Initial commit. Demo rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinventoryScratch::Application.config.secret_token = '75c4170062a051253b98eeacecf3e0556a27fb7a3240df1aa0b841a2c8e5a4b12f2f2bf3944da21027c67c4ba83ba15cd03274de1bf6ecc05f77153807b068af'
