6c3be9e823747f0b4ddc2fda0ebd992f3787e855
João Portela <joao.portela@gmail.com>
2011-10-10 23:45:00 +0100

    precompile

--- public/assets/application-d986bf8e6fe3063be06faedb2c1e6fb4.js
+++
@@ -1,3 +0,0 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-function toggle_holder(){elem=document.getElementById("item_status"),showProp=elem.value=="Requisitado"?"inline":"none",document.getElementById("holder_toggle").style.display=showProp}
