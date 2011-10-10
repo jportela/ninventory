c960450f9371c413f9e512322c36ec6e19d38b49
João Portela <joao.portela@gmail.com>
2011-10-10 22:24:40 +0100

    Corrigido erros de rails 2 para rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 NinventoryScratch::Application.routes.draw do
   root :to => 'items#index'
+  resources :items
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
