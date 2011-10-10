f997b9ccf52e174dddef7544aef5a79e7c3a7276
João Portela <joao.portela@gmail.com>
2011-10-10 22:54:22 +0100

    precompile of assets 2

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,10 @@ module NinventoryScratch
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.precompile += ['application.css']
+    config.assets.precompile += ['struct.css']
+    
+    
   end
 end
